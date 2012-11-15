0560d962a3e0bad32c0c6668cfdc2c2cf5d4c6cb
Stephen Freiberg <safreiberg@gmail.com>
2012-11-15 10:59:38 -0500

    homepage for unauth'd users

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 OctoLana::Application.routes.draw do  
   authenticated :user do
-    root :to => 'home#index'
+    root :to => 'home#auth'
   end
   root :to => "home#index"
   devise_for :users
   resources :users
+  
 end
