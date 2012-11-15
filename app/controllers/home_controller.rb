7d6b943c88c0d63f0c9da57bbc26d9405ec62c9f
Stephen Freiberg <safreiberg@gmail.com>
2012-11-15 17:02:04 -0500

    more heroku pg fixes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -9,7 +9,9 @@ class HomeController < ApplicationController
     @foods = current_user.foods.where('time BETWEEN ? AND ?', DateTime.now.beginning_of_day, DateTime.now.end_of_day).all
     @workouts = current_user.workouts.where('start BETWEEN ? AND ?', DateTime.now.beginning_of_day, DateTime.now.end_of_day).all
     @feelings = current_user.weathers.where('time BETWEEN ? AND ?', DateTime.now.beginning_of_day, DateTime.now.end_of_day).all
-    @sleeps = current_user.sleeps.where('(end BETWEEN ? AND ?) OR (start BETWEEN ? AND ?)', DateTime.now.beginning_of_day.strftime('%Y-%m-%d %H:%M:%S'), DateTime.now.end_of_day.strftime('%Y-%m-%d %H:%M:%S'), DateTime.now.beginning_of_day.strftime('%Y-%m-%d %H:%M:%S'), DateTime.now.end_of_day.strftime('%Y-%m-%d %H:%M:%S')).all
+    @sleeps = current_user.sleeps.where('end BETWEEN ? AND ?', DateTime.now.beginning_of_day, DateTime.now.end_of_day).all
+    @sleeps = @sleeps + current_user.sleeps.where('start BETWEEN ? AND ?',  DateTime.now.beginning_of_day, DateTime.now.end_of_day).all
+    @sleeps = @sleeps.uniq
     @healths = current_user.healths.where('time BETWEEN ? AND ?', DateTime.now.beginning_of_day, DateTime.now.end_of_day).all
     @events = current_user.healths.where('time BETWEEN ? AND ?', DateTime.now.beginning_of_day, DateTime.now.end_of_day).all
     @notes = current_user.notes.where('time BETWEEN ? AND ?', DateTime.now.beginning_of_day, DateTime.now.end_of_day).all
