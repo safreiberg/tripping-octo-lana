dfe06a0e846e98145f0c4bf7b5e9eef09dcf8e84
Stephen Freiberg <safreiberg@gmail.com>
2012-11-16 14:45:24 -0500

    added lots of views and controllers, but no actual functionality. also added feelings to homepage

---
+++ app/controllers/sleeps_controller.rb
@@ -0,0 +1,22 @@
+class SleepsController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  def new
+  end
+
+  def create
+  end
+end
